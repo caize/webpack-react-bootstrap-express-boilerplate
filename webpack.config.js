c1b989137210ae9ec1c8573399c110719f30fb75
Jacob Swanson <jacob@swandiggy.com>
2016-07-21 17:03:08 -0500

    Make output bundle have hash

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ],
     output: {
         path: path.join(__dirname, '/dist/'),
-        filename: 'bundle.js',
+        filename: 'bundle.[hash].js',
         publicPath: '/'
     },
     plugins: [
